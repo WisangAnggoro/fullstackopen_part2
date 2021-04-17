161f0bac4762b6049324c6929cd462d7ae6d8969
Wisang Jati Anggoro <wisang1999@gmail.com>
2021-04-17 22:09:10 +0700

    declare Course component as a separate module

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,58 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts.map((part) => part.exercises).reduce((a, b) => a + b, 0)
-  return(
-    <p><b>Total of {sum} exercise</b></p>
-  ) 
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {
-        course.parts.map((part, index) => 
-          <Part 
-            part={part} 
-            key={part.id} 
-          />
-        )
-      }
-      <Total course={course}/>
-    </div>
-  )
-}
-
-const Course = ({courses}) => {
-  return (
-    <div>
-      <h1>Web development curiculum</h1>
-      {
-        courses.map((course, index) =>
-          <div key={course.id}>
-            <Header course={course} />
-            <Content course={course} />
-          </div>
-        )
-      }
-    </div>
-  )
-}
+import Course from './Course'
 
 const App = () => {
   const courses = [
